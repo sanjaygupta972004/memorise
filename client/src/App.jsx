5b8a604ab3b3a6c4b205b2fcd214313053d8efaa
sanjaygupta972004 <sanjaygupta07054@gmail.com>
2023-12-22 22:52:40 +0530

    add vite app for frontend and add react-router-dom for routing

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'  
+import About from './pages/About'
+import Service from './pages/Service'
+import Contact from './pages/Contact'
+
+
+function App() {
+
+  return (
+    <>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/service" element={<Service />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </Router>
+
+    </>
+  )
+}
+
+export default App
